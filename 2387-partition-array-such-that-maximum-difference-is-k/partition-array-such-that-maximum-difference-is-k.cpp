f2fdf58890fb12462f0bf4975139370dabadf25b
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-19 20:56:04 +0530

    Time: 35 ms (61.24%) | Memory: 86.8 MB (96.08%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -5,15 +5,15 @@ public:
         sort(nums.begin(), nums.end(), [](int& a, int& b) {
             return a < b;
         });
-        nums.push_back(1000000);
+        // nums.push_back(1000000);
 
         int smallest = nums[0], result = 0, i = 0;
         while (i < n) {
-            while (nums[i] - smallest <= k) {
+            while (i < n && nums[i] - smallest <= k) {
                 i++;
             }
             result += 1;
-            smallest = nums[i];
+            if (i != n) smallest = nums[i];
         }
 
 
