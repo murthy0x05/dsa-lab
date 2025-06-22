69b8f7a03e44395202660798e5fd5ea829b158a8
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-22 10:26:42 +0530

    Time: 1 ms (93.67%) | Memory: 42.5 MB (53.61%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,17 +1,42 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
+        int n = s.length(), size = n / k;
         List<String> result = new ArrayList<>();
-        int start = 0, n = s.length();
-        while (start < n) {
-            if (start + k < n) {
-                result.add(s.substring(start, start + k));
-            } else {
-                result.add(String.format("%-" + k + "s", s.substring(start)));
+
+        for (int i = 0; i < size; i++) {
+            StringBuilder group = new StringBuilder();
+            for (int j = 0; j < k; j++) {
+                group.append(s.charAt(k*i + j));
             }
-            start += k;
+            result.add(group.toString());
         }
-        result.set(result.size() - 1, result.get(result.size() - 1).replace(' ', fill));
+
+        if (size * k < n) {
+            StringBuilder lastGroup = new StringBuilder(s.substring(size * k));
+            lastGroup.append(String.valueOf(fill).repeat(k - n + size * k));
+            result.add(lastGroup.toString());
+
+        }
 
         return result.toArray(new String[0]);
     }
 }
+
+/* 8ms Solution */
+// class Solution {
+//     public String[] divideString(String s, int k, char fill) {
+//         List<String> result = new ArrayList<>();
+//         int start = 0, n = s.length();
+//         while (start < n) {
+//             if (start + k < n) {
+//                 result.add(s.substring(start, start + k));
+//             } else {
+//                 result.add(String.format("%-" + k + "s", s.substring(start)));
+//             }
+//             start += k;
+//         }
+//         result.set(result.size() - 1, result.get(result.size() - 1).replace(' ', fill));
+
+//         return result.toArray(new String[0]);
+//     }
+// }
