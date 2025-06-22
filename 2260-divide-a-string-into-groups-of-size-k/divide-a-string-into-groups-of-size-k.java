cd81b5291148b1cfcf3287ba3a04fed833a0e2bc
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-22 10:38:17 +0530

    Time: 1 ms (93.67%) | Memory: 42 MB (94.88%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,26 +1,43 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        int n = s.length(), size = n / k;
-        List<String> result = new ArrayList<>();
+        int n = s.length(), size = (int)Math.ceil(n / (float)k);
+        String[] result = new String[size];
 
         for (int i = 0; i < size; i++) {
-            StringBuilder group = new StringBuilder();
-            for (int j = 0; j < k; j++) {
-                group.append(s.charAt(k*i + j));
-            }
-            result.add(group.toString());
+            result[i] = s.substring(i * k, Math.min(i * k + k, n));
+        }
+        if (result[size-1].length() < k) {
+            result[size-1] += String.valueOf(fill).repeat(k - result[size - 1].length());
         }
 
-        if (size * k < n) {
-            StringBuilder lastGroup = new StringBuilder(s.substring(size * k));
-            lastGroup.append(String.valueOf(fill).repeat(k - n + size * k));
-            result.add(lastGroup.toString());
-
-        }
-
-        return result.toArray(new String[0]);
+        return result;
     }
 }
+
+/* 1ms Solution */
+// class Solution {
+//     public String[] divideString(String s, int k, char fill) {
+//         int n = s.length(), size = n / k;
+//         List<String> result = new ArrayList<>();
+
+//         for (int i = 0; i < size; i++) {
+//             StringBuilder group = new StringBuilder();
+//             for (int j = 0; j < k; j++) {
+//                 group.append(s.charAt(k*i + j));
+//             }
+//             result.add(group.toString());
+//         }
+
+//         if (size * k < n) {
+//             StringBuilder lastGroup = new StringBuilder(s.substring(size * k));
+//             lastGroup.append(String.valueOf(fill).repeat(k - n + size * k));
+//             result.add(lastGroup.toString());
+
+//         }
+
+//         return result.toArray(new String[0]);
+//     }
+// }
 
 /* 8ms Solution */
 // class Solution {
