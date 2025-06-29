45fc338e06b8b313475dc460ee3b292eee27affd
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-29 18:38:53 +0530

    Time: 33 ms (46.45%) | Memory: 54.2 MB (56.00%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    const int MOD = 1e9 + 7;
+    vector<int> preComputeTwoPower(int n) {
+        vector<int> twoPower(n);
+        twoPower[0] = 1;
+        for (int i = 1; i < n; i++) {
+            twoPower[i] = (twoPower[i - 1] * 2LL) % MOD;
+        }
+        return twoPower;
+    }
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long result = 0;
+        sort(nums.begin(), nums.end());
+
+        vector<int> twoPower = preComputeTwoPower(n);
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            if (nums[start] + nums[end] > target) {
+                end -= 1;
+            } else {
+                result = (result + twoPower[end - start]) % MOD;
+                start += 1;
+            }
+        }
+
+        return result;
+    }
+};
