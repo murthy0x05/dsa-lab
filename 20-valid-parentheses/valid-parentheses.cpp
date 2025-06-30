9db4a9d66c0a9902eb8114711b0b2525baae5cd0
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-30 09:30:56 +0530

    Time: 3 ms (7.44%) | Memory: 9 MB (20.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        unordered_map<char, char> map;
+        map[')'] = '(';
+        map['}'] = '{';
+        map[']'] = '[';
+
+        stack<char> st;
+        for (int i = 0; i < n; i++) {
+            if (map.count(s[i])) {
+                if (!st.empty() && st.top() == map[s[i]]) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                st.push(s[i]);
+            }
+        }
+
+        return st.empty();
+    }
+};
