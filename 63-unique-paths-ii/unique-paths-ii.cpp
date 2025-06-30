66cd9680ba96f29d2ede543171923dd8da68486b
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-06-30 14:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.03%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -19,12 +19,6 @@ public:
             }
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
-        }
         return dp[rows - 1][cols - 1];
     }
 };
