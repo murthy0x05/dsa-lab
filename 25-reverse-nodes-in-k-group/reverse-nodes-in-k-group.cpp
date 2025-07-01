5887a9996c2748444105732bfecfab62f06efc30
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-01 21:58:28 +0530

    Time: 37 ms (1.72%) | Memory: 16.5 MB (70.89%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    void reverse(ListNode* head) {
+        if (head == nullptr) {
+            return ;
+        }
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+        while (curr) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return ;
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* prevTail = nullptr;
+        ListNode* nextHead = head;
+        ListNode* temp = head;
+        for (int i = 0; i < k - 1; i++) {
+            temp = temp -> next;
+        }
+        nextHead = temp -> next;
+        temp -> next = nullptr;
+        reverse(head);
+        ListNode* result = temp;
+        prevTail = head;
+
+        while (nextHead != nullptr) {
+            temp = nextHead;
+            for (int i = 0; temp != nullptr && i < k - 1; i++) {
+                temp = temp -> next;
+            }
+            if (temp != nullptr) {
+                ListNode* currHead = nextHead;
+                nextHead = temp -> next;
+                temp -> next = nullptr;
+                reverse(currHead);
+                prevTail -> next = temp;
+                prevTail = currHead;
+            } else {
+                prevTail -> next = nextHead;
+                nextHead = nullptr;
+                ListNode* curr = result;
+                while (curr) {
+                    cout << curr -> val << " ";
+                    curr = curr -> next;
+                }
+                cout << endl;
+            }
+        }
+
+
+        return result;
+    }
+};
