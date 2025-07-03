80e94dbda4746b4bdd5a54a83c246f1fb46c1830
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-03 09:35:59 +0530

    Time: 14 ms (27.79%) | Memory: 29.7 MB (38.78%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -9,86 +9,92 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// private:
+//     TreeNode* treeRoot = nullptr;
+// public:
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum = 0) {
+//         if (!treeRoot) this -> treeRoot = root;
+//         if (!root) return {};
+//         if (!root -> left && !root -> right) {
+//             if (targetSum == (currSum + root -> val)) {
+//                 return {{root -> val}};
+//             } else {
+//                 return {};
+//             }
+//         }
+
+//         vector<vector<int>> leftTree = pathSum(root -> left, targetSum, currSum + root -> val);
+//         vector<vector<int>> rightTree = pathSum(root -> right, targetSum, currSum + root -> val);
+
+//         if (leftTree.size() > 0) {
+//             for (vector<int>& tree: leftTree) {
+//                 tree.push_back(root -> val);
+//             }
+//         }
+
+//         if (rightTree.size() > 0) {
+//             for (vector<int>& tree: rightTree) {
+//                 tree.push_back(root -> val);
+//             }
+//         }
+
+//         vector<vector<int>> result;
+//         for (vector<int> tree: leftTree) {
+//             if (tree.size()) result.push_back(tree);
+//         }
+//         for (vector<int> tree: rightTree) {
+//             if (tree.size()) result.push_back(tree);
+//         }
+
+//         if (root == treeRoot) {
+//             for (vector<int>& list: result) {
+//                 int start = 0, end = list.size() - 1;
+//                 while (start < end) {
+//                     list[start] = list[end] + list[start];
+//                     list[end] = list[start] - list[end];
+//                     list[start] = list[start] - list[end];
+//                     start += 1; end -= 1;
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
 class Solution {
-private:
-    TreeNode* treeRoot = nullptr;
-public:
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum = 0) {
-        if (!treeRoot) this -> treeRoot = root;
-        if (!root) return {};
-        if (!root -> left && !root -> right) {
-            if (targetSum == (currSum + root -> val)) {
-                return {{root -> val}};
-            } else {
-                return {};
-            }
-        }
-
-        vector<vector<int>> leftTree = pathSum(root -> left, targetSum, currSum + root -> val);
-        vector<vector<int>> rightTree = pathSum(root -> right, targetSum, currSum + root -> val);
-
-        if (leftTree.size() > 0) {
-            for (vector<int>& tree: leftTree) {
-                tree.push_back(root -> val);
-            }
-        }
-
-        if (rightTree.size() > 0) {
-            for (vector<int>& tree: rightTree) {
-                tree.push_back(root -> val);
-            }
-        }
-
+    vector<vector<int>> solve(TreeNode* root, vector<int>& path, int currSum, int& targetSum) {
+        currSum += root -> val;
+        path.push_back(root -> val);
         vector<vector<int>> result;
-        for (vector<int> tree: leftTree) {
-            if (tree.size()) result.push_back(tree);
-        }
-        for (vector<int> tree: rightTree) {
-            if (tree.size()) result.push_back(tree);
-        }
-
-        if (root == treeRoot) {
-            for (vector<int>& list: result) {
-                int start = 0, end = list.size() - 1;
-                while (start < end) {
-                    list[start] = list[end] + list[start];
-                    list[end] = list[start] - list[end];
-                    list[start] = list[start] - list[end];
-                    start += 1; end -= 1;
-                }
+        if (!root -> left && !root -> right) {
+            if (targetSum == currSum) {
+                result.push_back(path);
             }
         }
+        if (root -> left) {
+            vector<vector<int>> leftResult = solve(root -> left, path, currSum, targetSum);
+            for (vector<int>& tree: leftResult) {
+                result.push_back(tree);
+            }
+        }
+        if (root -> right) {
+            vector<vector<int>> rightResult = solve(root -> right, path, currSum, targetSum);
+            for (vector<int>& tree: rightResult) {
+                result.push_back(tree);
+            }
+        }
+        path.pop_back();
 
         return result;
     }
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum = 0) {
+        if (!root) {
+            return {};
+        }
+        vector<int> path;
+        return solve(root, path, 0, targetSum);
+    }
 };
-
-// class Solution {
-//     vector<vector<int>> solve(TreeNode* root, vector<int>& path, int currSum, int& targetSum) {
-//         currSum += root -> val;
-//         path.push_back(root -> val);
-//         vector<vector<int>> result;
-//         if (!root -> left && !root -> right) {
-//             if (targetSum == currSum) {
-//                 result.push_back(path);
-//             }
-//         }
-//         if (root -> left) {
-//             vector<vector<int>> leftResult = solve(root -> left, path, currSum, targetSum);
-//         }
-//         if (root -> right) {
-//             vector<vector<int> rightResult = solve(root -> right, path, currSum, targetSum);
-//         }
-//         for (vector<int>& tree: leftResult) {
-
-//         }
-//     }
-// public:
-//     vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum = 0) {
-//         if (!root) {
-//             return {};
-//         }
-//         vector<int> path;
-//         return solve(root, path, 0, targetSum);
-//     }
-// };
