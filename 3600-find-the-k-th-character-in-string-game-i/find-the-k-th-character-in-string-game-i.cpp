4461f5b1674745303898582f1e7f5b4bf26f7997
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-03 20:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.34%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        if (k == 1) return 'a';
-        int currSize = 1;
-        string prevWord = "a";
-        while (currSize < k) {
-            string currWord(prevWord);
-            int i = 0, len = prevWord.size();
-            if (currSize + len >= k) break;
-            while (i < len) {
-                currWord.push_back(((prevWord[i] - 'a' + 1) % 26) + 'a');
-                i += 1;
-            }
-            currSize += len;
-            prevWord = currWord;
-        }
-        return (((prevWord[k - currSize - 1] - 'a' + 1) % 26) + 'a');
+        return __builtin_popcount(k - 1) + 'a';
     }
 };
