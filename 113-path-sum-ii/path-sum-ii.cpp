17cd0d96754b0ac42fcc5691715c5ae55174399a
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-03 09:23:08 +0530

    Time: 10 ms (36.85%) | Memory: 36.2 MB (32.78%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* treeRoot = nullptr;
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum = 0) {
+        if (!treeRoot) this -> treeRoot = root;
+        if (!root) return {};
+        if (!root -> left && !root -> right) {
+            if (targetSum == (currSum + root -> val)) {
+                return {{root -> val}};
+            } else {
+                return {};
+            }
+        }
+
+        vector<vector<int>> leftTree = pathSum(root -> left, targetSum, currSum + root -> val);
+        vector<vector<int>> rightTree = pathSum(root -> right, targetSum, currSum + root -> val);
+
+        if (leftTree.size() > 0) {
+            for (vector<int>& tree: leftTree) {
+                tree.push_back(root -> val);
+            }
+        }
+
+        if (rightTree.size() > 0) {
+            for (vector<int>& tree: rightTree) {
+                tree.push_back(root -> val);
+            }
+        }
+
+        vector<vector<int>> result;
+        for (vector<int> tree: leftTree) {
+            if (tree.size()) result.push_back(tree);
+        }
+        for (vector<int> tree: rightTree) {
+            if (tree.size()) result.push_back(tree);
+        }
+
+        if (root == treeRoot) {
+            for (vector<int>& list: result) {
+                int start = 0, end = list.size() - 1;
+                while (start < end) {
+                    list[start] = list[end] + list[start];
+                    list[end] = list[start] - list[end];
+                    list[start] = list[start] - list[end];
+                    start += 1; end -= 1;
+                }
+            }
+        }
+
+        return result;
+    }
+};
