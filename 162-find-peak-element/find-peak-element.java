241ab37e805767cf7d0e564f82120c362adc7147
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-06 19:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (42.80%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -10,16 +10,14 @@ class Solution {
             int low = 1, high = n - 2;
             while (low <= high) {
                 int mid = low + ((high - low) >> 1);
-                if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
-                    return mid;
-                } else if (nums[mid] > nums[mid +  1]) {
-                    high = mid - 1;
-                } else {
+                if (nums[mid] < nums[mid + 1]) {
                     low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
 
-            return -1;
+            return low;
         }
     }
 }
