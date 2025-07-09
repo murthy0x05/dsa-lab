e19401be2df9613ddc0a9220479a7ae217f37915
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-09 09:20:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (61.37%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -7,22 +7,17 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i - 1 >= 0 && j - 1 >= 0) {
-                    dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
-                } else if (i - 1 >= 0) {
-                    dp[i][j] = grid[i][j] + dp[i - 1][j];
-                } else if (j - 1 >= 0) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
                     dp[i][j] = grid[i][j] + dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = grid[i][j] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        // for (auto& row: dp) {
-        //     for (int ele: row) {
-        //         cout << ele << ' ';
-        //     }
-        //     cout << endl;
-        // }
 
         return dp.back().back();
     }
