f3a8241c51a6d3ba3e6ae7dce7563e7a2753888c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-09 10:25:27 +0530

    Time: 16 ms (12.63%) | Memory: 25.9 MB (35.63%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    unordered_map<TreeNode*, pair<int, int>> mem;
+    int doDFS(TreeNode* root, bool canInclude = true) {
+        if (!root) return 0;
+
+        if (mem.count(root)) {
+            if (canInclude == true && mem[root].first > 0) {
+                return mem[root].first;
+            }
+            if (canInclude == false && mem[root].second > 0) {
+                return mem[root].second;
+            }
+        }
+        int include = 0;
+        if (canInclude)
+            include = root -> val + doDFS(root -> left, false) + doDFS(root -> right, false);
+        int exclude = doDFS(root -> left) + doDFS(root -> right);
+
+        mem[root] = make_pair(-1, -1);
+        if (canInclude == true) {
+            return mem[root].first = max(include, exclude);
+        } else {
+            return mem[root].second = max(include, exclude);
+        }
+    }
+public:
+    int rob(TreeNode* root) {
+        return doDFS(root);
+    }
+};
