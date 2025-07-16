5d7fe341637a76db063ec349e32c23e1768c7f39
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-16 15:26:32 +0530

    Time: 80 ms (74.75%) | Memory: 44.3 MB (80.18%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+    inline int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> adjlist(n);
         for (auto& edge: times)
             adjlist[edge[0] - 1].push_back({edge[1] - 1, edge[2]});
@@ -8,12 +8,12 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         vector<int> distance(n, INT_MAX);
         distance[k - 1] = 0;
-        pq.push({k - 1, 0});
+        pq.push({0, k - 1});
 
         while (!pq.empty()) {
             pair<int, int> curr = pq.top(); pq.pop();
-            int from = curr.first;
-            if (curr.second > distance[from])
+            int from = curr.second;
+            if (curr.first > distance[from])
                 continue;
             else {
                 for (auto& neighbour: adjlist[from]) {
@@ -21,13 +21,13 @@ public:
                     int weight = neighbour.second;
                     if (distance[from] + weight < distance[to]) {
                         distance[to] = distance[from] + weight;
-                        pq.push({to, distance[to]});
+                        pq.push({distance[to], to});
                     }
                 }
             }
         }
 
         int max_ele = *max_element(distance.begin(), distance.end());
-        return max_ele > 600000 ? -1 : max_ele;
+        return max_ele == INT_MAX ? -1 : max_ele;
     }
 };
