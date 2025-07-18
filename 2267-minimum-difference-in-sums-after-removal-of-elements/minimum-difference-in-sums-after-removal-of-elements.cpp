60ede4197edb85c5cba1cf9c29cbda41355294df
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-19 01:02:11 +0530

    Time: 134 ms (77.58%) | Memory: 141.4 MB (89.95%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -2,17 +2,17 @@ class Solution {
     #define ll long long
 public:
     long long minimumDifference(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(), x = n / 3;
 
         priority_queue<int, vector<int>, less<int>> maxheap;
         vector<ll> left_mins;
         ll left_sum = 0;
-        for (int i = 0; i < (n / 3); i++) {
+        for (int i = 0; i < x; i++) {
             maxheap.push(nums[i]);
             left_sum += nums[i];
         }
         left_mins.push_back(left_sum);
-        for (int i = n / 3; i < 2 * (n / 3); i++) {
+        for (int i = n / 3; i < 2 * x; i++) {
             maxheap.push(nums[i]);
             left_sum += nums[i];
             left_sum -= maxheap.top();
@@ -22,14 +22,14 @@ public:
 
         priority_queue<int, vector<int>, greater<int>> minheap;
         ll right_sum = 0;
-        for (int i = n - 1; i >= 2 * (n / 3); i--) {
+        for (int i = n - 1; i >= 2 * x; i--) {
             minheap.push(nums[i]);
             right_sum += nums[i];
         }
 
         ll min_diff = LLONG_MAX;
-        for (int i = 2 * (n / 3) - 1; i >= (n / 3); i--) {
-            min_diff = min(min_diff, left_mins[i - (n / 3 - 1)] - right_sum);
+        for (int i = 2 * x - 1; i >= x; i--) {
+            min_diff = min(min_diff, left_mins[i - x + 1] - right_sum);
             minheap.push(nums[i]);
             right_sum += nums[i];
             right_sum -= minheap.top();
