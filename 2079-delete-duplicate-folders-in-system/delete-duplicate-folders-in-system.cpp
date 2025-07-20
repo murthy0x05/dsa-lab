809e523ea835ec4daeebf23dde9eb8dce1c5af93
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-21 01:52:20 +0530

    Time: 225 ms (62.50%) | Memory: 253.3 MB (86.25%) - LeetSync

---
+++ 2079-delete-duplicate-folders-in-system/delete-duplicate-folders-in-system.cpp
@@ -0,0 +1,90 @@
+struct TrieNode {
+    string folder;
+    bool remove = false;
+    map<string, TrieNode*> child;
+    TrieNode(string _folder) {
+        this -> folder = _folder;
+        this -> remove = false;
+    }
+};
+class Trie {
+    #define psb push_back
+    #define ppb pop_back
+public:
+    TrieNode* root = nullptr;
+    Trie() {
+        root = new TrieNode("/");
+    }
+
+    void insert(const vector<string>& path) {
+        TrieNode* curr = root;
+        for (const string& folder: path) {
+            if ((curr -> child).find(folder) == (curr -> child).end()) {
+                curr -> child[folder] = new TrieNode(folder);
+            }
+            curr = curr -> child[folder];
+        }
+    }
+    string mark_repeating(map<string, TrieNode*>& paths, TrieNode* root) {
+        string subfolder("");
+        for (auto& node: root -> child) {
+            subfolder += mark_repeating(paths, node.second);
+        }
+
+        if (subfolder.size() != 0) {
+            if (paths.find(subfolder) != paths.end()) {
+                paths[subfolder] -> remove = true;
+                root -> remove = true;
+            } else {
+                paths[subfolder] = root;
+            }
+        }
+
+        return "[" + root -> folder + subfolder + "]";
+    }
+    bool doDFS(auto& result, auto& curr, TrieNode* root) {
+        if (root -> remove) {
+            return false;
+        }
+
+        for (auto& node: root -> child) {
+            curr.psb(node.first);
+            if (doDFS(result, curr, node.second)) {
+                result.psb(curr);
+            }
+            curr.ppb();
+        }
+
+        return true;
+    }
+    vector<vector<string>> delete_and_return() {
+        map<string, TrieNode*> paths;
+        for (auto& [folder, node]: root -> child) {
+            mark_repeating(paths, node);
+        }
+
+        vector<vector<string>> result;
+        vector<string> curr;
+        for (auto& node: root -> child) {
+            curr.psb(node.first);
+            if (doDFS(result, curr, node.second)) {
+                result.psb(curr);
+            }
+            curr.ppb();
+        }
+
+        return result;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
+        Trie trie;
+        for (auto& path: paths) {
+            trie.insert(path);
+        }
+
+        return trie.delete_and_return();
+    }
+};
