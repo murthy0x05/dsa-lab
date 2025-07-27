3a3e3505e4559b55dae536a746fd0b59253466c7
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-28 00:47:37 +0530

    Time: 7 ms (59.40%) | Memory: 14.5 MB (17.12%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -13,9 +13,9 @@ class Solution {
             return mem[i][j] = solve(i - 1, j - 1);
         } else {
             return mem[i][j] = 1 + min({
-                solve(i - 1, j),
-                solve(i, j - 1),
-                solve(i - 1, j - 1)
+                solve(i - 1, j), // case of removal
+                solve(i, j - 1), // case of insertion
+                solve(i - 1, j - 1) // case of replace
             });
         }
     }
@@ -25,13 +25,6 @@ public:
         this -> s2 = word2;
         int n = s1.size(), m = s2.size();
         mem.assign(n, vector<int>(m, -1));
-        solve(n - 1, m - 1);
-        if (n == 0) {
-            return m;
-        } else if (m == 0) {
-            return n;
-        } else {
-            return mem[n - 1][m - 1];
-        }
+        return solve(n - 1, m - 1);
     }
 };
