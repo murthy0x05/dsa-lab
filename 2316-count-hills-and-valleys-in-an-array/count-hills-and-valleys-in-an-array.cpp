ceee0d9f12249749803f8fefd6a7fc12c242552e
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-27 21:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (50.25%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -3,14 +3,14 @@ public:
     int countHillValley(vector<int>& nums) {
         int n = nums.size(), size = 0;
         int answer = 0;
-        int left_closest = 0;
+        int left_closest = nums[0];
         for (int i = 1; i < n - 1; i++) {
             if (nums[i] != nums[i + 1]) {
-                if (nums[i] < nums[left_closest] && nums[i] < nums[i + 1] ||
-                    nums[i] > nums[left_closest] && nums[i] > nums[i + 1]) {
+                if (nums[i] < left_closest && nums[i] < nums[i + 1] ||
+                    nums[i] > left_closest && nums[i] > nums[i + 1]) {
                     answer++;
                 }
-                left_closest = i;
+                left_closest = nums[i];
             }
         }
     
