d340e4cc29898431cb96666c314d62e9893ce0de
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-28 00:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (99.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -3,26 +3,26 @@ public:
     int minDistance(string &s1, string &s2) {
         int n = s1.size(), m = s2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = i;
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = j;
+        vector<int> prev(m + 1), curr(m + 1);
+        for (int i = 0; i <= m; i++)
+            prev[i] = i;
         
         for (int i = 1; i <= n; i++) {
+            curr[0] = i;
             for (int j = 1; j <= m; j++) {
                 if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 } else {
-                    dp[i][j] = 1 + min({
-                        dp[i - 1][j], // case of removal
-                        dp[i][j - 1], // case of insertion
-                        dp[i-1][j-1] // case of replace
+                    curr[j] = 1 + min({
+                        prev[j], // case of removal
+                        curr[j - 1], // case of insertion
+                        prev[j-1] // case of replace
                     });
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
