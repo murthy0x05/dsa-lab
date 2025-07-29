c46a235a8c09d4c8baa3fb26e0de02fdef2943a6
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-07-30 02:51:53 +0530

    Time: 4 ms (38.22%) | Memory: 10.1 MB (75.49%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,5 +1,27 @@
 class Solution {
-    unordered_set<int> cols, pos_diag, neg_diag;
+    bool isSafe(auto& state, int& row, int& col, int& n) {
+        // column check
+        for (int i = 0; i < row; i++) {
+            if (state[i][col] == 'Q')
+                return false;
+        }
+
+        // negative diagonal check
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (state[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // positive diagonal check
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (state[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     void backtrack(auto& states, auto& state, int& n, int row) {
         if (row == n) {
             states.push_back(state);
@@ -7,22 +29,10 @@ class Solution {
         }
 
         for (int col = 0; col < n; col++) {
-            if (0 == cols.count(col) &&
-                0 == neg_diag.count(row - col) &&
-                0 == pos_diag.count(row + col)) {
-    
-                cols.insert(col);
-                neg_diag.insert(row - col);
-                pos_diag.insert(row + col);
+            if (isSafe(state, row, col, n)) {
                 state[row][col] = 'Q';
-                
                 backtrack(states, state, n, row + 1);
-
-                cols.erase(col);
-                neg_diag.erase(row - col);
-                pos_diag.erase(row + col);
                 state[row][col] = '.';
-
             }
         }
     }
@@ -34,7 +44,5 @@ public:
         backtrack(states, state, n, 0);
         
         return states;
-
-
     }
 };
