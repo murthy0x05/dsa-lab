f181c796aeeab89745031e6131adc56d052596aa
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-13 22:47:35 +0530

    Time: 68 ms (5.00%) | Memory: 10.2 MB (98.86%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,14 +3,10 @@ public:
     string frequencySort(string s) {
         unordered_map<char, int> freq;
         for (const char& c: s) {
-            if (isdigit(c)) {
-                freq[c]++;
-            } else {
-                freq[c]++;
-            }
+            freq[c]++;
         }
 
-        sort(s.begin(), s.end(), [&freq](const char a, const char b) {
+        sort(s.begin(), s.end(), [&freq](const char& a, const char& b) {
             if (freq[a] != freq[b]) {
                 return freq[a] > freq[b];
             } else {
