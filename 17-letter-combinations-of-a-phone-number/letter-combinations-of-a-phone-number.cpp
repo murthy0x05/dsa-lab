a0ef5789dad723d9571a2c5371c21c94d696d58c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-15 17:21:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (75.08%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,17 @@
 class Solution {
     vector<string> result;
     string digits; int n;
-    string letters(int n) {
-        if (n == 2) return "abc";
-        if (n == 3) return "def";
-        if (n == 4) return "ghi";
-        if (n == 5) return "jkl";
-        if (n == 6) return "mno";
-        if (n == 7) return "pqrs";
-        if (n == 8) return "tuv";
-        if (n == 9) return "wxyz";
-
-        return "";
-    }
+    vector<string> letters = {"abc", "def",
+                              "ghi", "jkl",
+                              "mno", "pqrs",
+                              "tuv", "wxyz"};
     void backtrack(string& combination, int i) {
         if (i == n) {
             result.push_back(combination);
             return ;
         }
 
-        for (const char& c: letters(digits[i] - '0')) {
+        for (const char& c: letters[digits[i] - '2']) {
             combination.push_back(c);
             backtrack(combination, i + 1);
             combination.pop_back();
@@ -27,9 +19,11 @@ class Solution {
     }
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.size() == 0) return {}; 
+        if (digits.size() == 0) return {};
+
         this -> digits = digits;
         n = digits.size();
+
         string combination("");
         backtrack(combination, 0);
 
