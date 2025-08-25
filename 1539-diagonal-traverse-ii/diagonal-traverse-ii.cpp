0f69ce6fffa66cbd5d653112e02f929bd12a415b
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-26 02:11:50 +0530

    Time: 19 ms (74.48%) | Memory: 69.9 MB (95.87%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,32 +1,61 @@
+// class Solution {
+//     struct state {
+//         int val, i, j;
+//         state(int v, int i, int j) : val(v), i(i), j(j) {}
+//     };
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+//         int r = nums.size();
+
+//         auto cmp = [](const state& a, const state& b) {
+//             if ((a.i + a.j) != (b.i + b.j)) {
+//                 return (a.i + a.j) > (b.i + b.j);
+//             } else {
+//                 return a.i < b.i;
+//             }
+//         };
+//         priority_queue<state, vector<state>, decltype(cmp)> pq(cmp);
+
+//         for (int i = 0; i < r; i++) {
+//             for (int j = 0, c = nums[i].size(); j < c; j++) {
+//                 // Return Value Optimization(RVO)
+//                 pq.push(state(nums[i][j], i, j));
+//             }
+//         }
+
+//         vector<int> flatend;
+//         while (!pq.empty()) {
+//             flatend.push_back(pq.top().val);
+//             pq.pop();
+//         }
+
+//         return flatend;
+//     }
+// };
+
+
 class Solution {
     struct state {
-        int val, i, j;
-        state(int v, int i, int j) : val(v), i(i), j(j) {}
+        int i, j;
+        state(int i, int j) : i(i), j(j) {}
     };
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         int r = nums.size();
-
-        auto cmp = [](const state& a, const state& b) {
-            if ((a.i + a.j) != (b.i + b.j)) {
-                return (a.i + a.j) > (b.i + b.j);
-            } else {
-                return a.i < b.i;
-            }
-        };
-        priority_queue<state, vector<state>, decltype(cmp)> pq(cmp);
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0, c = nums[i].size(); j < c; j++) {
-                // Return Value Optimization(RVO)
-                pq.push(state(nums[i][j], i, j));
-            }
-        }
+        queue<state> q;
+        q.push(state(0, 0));
 
         vector<int> flatend;
-        while (!pq.empty()) {
-            flatend.push_back(pq.top().val);
-            pq.pop();
+        while (!q.empty()) {
+            state curr = q.front(); q.pop();
+            flatend.push_back(nums[curr.i][curr.j]);
+
+            if (0 == curr.j and curr.i + 1 <  r) {
+                q.push(state(curr.i + 1, curr.j));
+            }
+            if (curr.j + 1 < nums[curr.i].size()) {
+                q.push(state(curr.i, curr.j + 1));
+            }
         }
 
         return flatend;
