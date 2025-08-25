1b0d85032f4fb1713d52f2d7c005cca97ab6c960
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-25 18:31:38 +0530

    Time: 112 ms (30.54%) | Memory: 42.3 MB (97.55%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,41 +1,76 @@
+// class Solution {
+//     List<List<Integer>> mem;
+//     private int solve(int[] arr, int i, int j) {
+//         if (i == j) {
+//             mem.get(i).set(j, 0);
+//             return 0;
+//         }
+//         if (mem.get(i).get(j) != -1) {
+//             return mem.get(i).get(j);
+//         }
+
+//         int maxi = Integer.MIN_VALUE;
+//         for (int k = i; k <= j - 1; k++) {
+//             int cost = arr[i - 1] * arr[k] * arr[j] +
+//                        solve(arr, i, k) +
+//                        solve(arr, k + 1, j);
+//             maxi = Math.max(maxi, cost);
+//         } 
+
+//         mem.get(i).set(j, maxi);
+//         return maxi;
+//     }
+//     public int maxCoins(int[] nums) {
+//         int n = nums.length;
+//         mem = new ArrayList<>();
+//         for (int i = 0; i < n + 2; i++) {
+//             mem.add(new ArrayList<>());    
+//             for (int j = 0; j < n + 2; j++) {
+//                 mem.get(i).add(Integer.valueOf(-1));
+//             }
+//         }
+
+//         int[] arr = new int[n + 2];
+//         arr[0] = 1;
+//         arr[n + 1] = 1;
+//         for (int i = 1; i <= n; i++) {
+//             arr[i] = nums[i - 1];
+//         }
+
+//         return solve(arr, 1, n + 1);
+//     }
+// }
+
 class Solution {
-    List<List<Integer>> mem;
+    int[][] mem;
     private int solve(int[] arr, int i, int j) {
         if (i == j) {
-            mem.get(i).set(j, 0);
-            return 0;
+            return mem[i][j] = 0;
         }
-        if (mem.get(i).get(j) != -1) {
-            return mem.get(i).get(j);
+        if (mem[i][j] != -1) {
+            return mem[i][j];
         }
 
-        int maxi = Integer.MIN_VALUE;
+        int maxCoins = Integer.MIN_VALUE;
         for (int k = i; k <= j - 1; k++) {
-            int cost = arr[i - 1] * arr[k] * arr[j] +
+            int coins = arr[i - 1] * arr[k] * arr[j] +
                        solve(arr, i, k) +
                        solve(arr, k + 1, j);
-            maxi = Math.max(maxi, cost);
-        } 
+            maxCoins = Math.max(maxCoins, coins);
+        }
 
-        mem.get(i).set(j, maxi);
-        return maxi;
+        return mem[i][j] = maxCoins;
     }
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        mem = new ArrayList<>();
+        mem = new int[n + 2][n + 2];
         for (int i = 0; i < n + 2; i++) {
-            mem.add(new ArrayList<>());    
-            for (int j = 0; j < n + 2; j++) {
-                mem.get(i).add(Integer.valueOf(-1));
-            }
+            Arrays.fill(mem[i], -1);
         }
 
         int[] arr = new int[n + 2];
-        arr[0] = 1;
-        arr[n + 1] = 1;
-        for (int i = 1; i <= n; i++) {
-            arr[i] = nums[i - 1];
-        }
+        arr[0] = 1; arr[n + 1] = 1;
+        System.arraycopy(nums, 0, arr, 1, n);
 
         return solve(arr, 1, n + 1);
     }
