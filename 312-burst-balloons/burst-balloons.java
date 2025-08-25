4fe2a4bb4d42e89c3dae3114c4e77c0cb1cf94a6
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-25 18:23:23 +0530

    Time: 449 ms (5.03%) | Memory: 44.8 MB (5.34%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.java
@@ -0,0 +1,42 @@
+class Solution {
+    List<List<Integer>> mem;
+    private int solve(int[] arr, int i, int j) {
+        if (i == j) {
+            mem.get(i).set(j, 0);
+            return 0;
+        }
+        if (mem.get(i).get(j) != -1) {
+            return mem.get(i).get(j);
+        }
+
+        int maxi = Integer.MIN_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost = arr[i - 1] * arr[k] * arr[j] +
+                       solve(arr, i, k) +
+                       solve(arr, k + 1, j);
+            maxi = Math.max(maxi, cost);
+        } 
+
+        mem.get(i).set(j, maxi);
+        return maxi;
+    }
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        mem = new ArrayList<>();
+        for (int i = 0; i < n + 2; i++) {
+            mem.add(new ArrayList<>());    
+            for (int j = 0; j < n + 2; j++) {
+                mem.get(i).add(Integer.valueOf(-1));
+            }
+        }
+
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            arr[i] = nums[i - 1];
+        }
+
+        return solve(arr, 1, n + 1);
+    }
+}
