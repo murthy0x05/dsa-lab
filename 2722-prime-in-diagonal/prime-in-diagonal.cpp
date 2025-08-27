5e405429f4a61ae326a1da2ddf15bcb1c7044412
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-27 20:06:14 +0530

    Time: 8 ms (21.98%) | Memory: 39.7 MB (7.82%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/prime-in-diagonal.cpp
@@ -1,30 +1,31 @@
 class Solution {
     using ui = unsigned int;
+    bool isPrime(ui n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (ui i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
 public:
     int diagonalPrime(vector<vector<int>>& nums) {
-        int n = nums.size();
+        ui n = nums.size();
         
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            maxi = max<int>({maxi, nums[i][i], nums[i][n - i - 1]});
-        }
-
-        vector<bool> isPrime(maxi + 1, true);
-        isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i <= maxi; i++) {
-            if (isPrime[i]) {
-                for (ui j = i * (ui)i; j <= maxi; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-
-        int maxPrime = 0;
+        ui maxi = 0;
         for (ui i = 0; i < n; i++) {
-            if (isPrime[nums[i][i]] && maxPrime < nums[i][i]) {
+            maxi = max<ui>({maxi, (ui)nums[i][i], (ui)nums[i][n - i - 1]});
+        }
+
+        ui maxPrime = 0;
+        for (ui i = 0; i < n; i++) {
+            if (isPrime((ui)nums[i][i]) && maxPrime < nums[i][i]) {
                 maxPrime = nums[i][i];
             }
-            if (isPrime[nums[i][n - i - 1]] && maxPrime < nums[i][n - i - 1]) {
+            if (isPrime((ui)nums[i][n - i - 1]) && maxPrime < nums[i][n - i - 1]) {
                 maxPrime = nums[i][n - i - 1];
             }
         }
