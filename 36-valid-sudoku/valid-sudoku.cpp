f4f4dc96b16520eae2c198140b8aa9722e49c540
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-08-30 13:09:38 +0530

    Time: 3 ms (62.80%) | Memory: 22 MB (78.77%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,34 +1,34 @@
 class Solution {
     #define pii pair<int, int>
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        unordered_map<int, set<char>> rows;
-        unordered_map<int, set<char>> cols;
-        map<pii, set<char>> cube;
+    // bool isValidSudoku(vector<vector<char>>& board) {
+    //     int n = board.size();
+    //     unordered_map<int, set<char>> rows;
+    //     unordered_map<int, set<char>> cols;
+    //     map<pii, set<char>> cube;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    bool inRow = rows[i].find(num) != rows[i].end();
-                    bool inCol = cols[j].find(num) != cols[j].end();
-                    auto& currCube = cube[make_pair(i / 3, j / 3)];
-                    bool inCube = currCube.find(num) != currCube.end();
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             char num = board[i][j];
+    //             if (num != '.') {
+    //                 bool inRow = rows[i].find(num) != rows[i].end();
+    //                 bool inCol = cols[j].find(num) != cols[j].end();
+    //                 auto& currCube = cube[make_pair(i / 3, j / 3)];
+    //                 bool inCube = currCube.find(num) != currCube.end();
 
-                    if (inRow || inCol || inCube) {
-                        return false;
-                    } else {
-                        rows[i].insert(num);
-                        cols[j].insert(num);
-                        currCube.insert(num);
-                    }
-                }
-            };
-        };
+    //                 if (inRow || inCol || inCube) {
+    //                     return false;
+    //                 } else {
+    //                     rows[i].insert(num);
+    //                     cols[j].insert(num);
+    //                     currCube.insert(num);
+    //                 }
+    //             }
+    //         };
+    //     };
         
-        return true;
-    }
+    //     return true;
+    // }
 
     // bool isValidSudoku {
         // int n = board.size();
@@ -52,4 +52,47 @@ public:
 
         // return true;
     // }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for (int i = 0; i < 9; i++) {
+            vector<bool> seen(9, false);
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                if (seen[board[i][j] - '1']) {
+                    return false;
+                } else {
+                    seen[board[i][j] - '1'] = true;
+                }
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            vector<bool> seen(9, false);
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] == '.') continue;
+                if (seen[board[i][j] - '1']) {
+                    return false;
+                } else {
+                    seen[board[i][j] - '1'] = true;
+                }
+            }
+        }
+
+        for (int box = 0; box < 9; box++) {
+            int begin_i = (box / 3) * 3, begin_j = (box % 3) * 3;
+            vector<bool> seen(9, false);
+            for (int i = begin_i; i < begin_i + 3; i++) {
+                for (int j = begin_j; j < begin_j + 3; j++) {
+                    if (board[i][j] == '.') continue;
+                    if (seen[board[i][j] - '1']) {
+                        return false;
+                    } else {
+                        seen[board[i][j] - '1'] = true;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 };
