013f11225bcd97152e3e731c97d85e43ac20615e
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-09-09 18:04:03 +0530

    Time: 56 ms (8.42%) | Memory: 9.3 MB (84.16%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    const int MOD = 1e9 + 7;
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = i - delay; j >= i - forget + 1 && j > 0; j--) {
+                dp[i] = (dp[i] + dp[j]) % MOD;
+            }
+        }
+
+        int sum = 0;
+        for (int i = n - forget + 1; i <= n; i++) {
+            cout << dp[i] << endl;
+            sum = (sum + dp[i]) % MOD;
+        }
+
+        return sum;
+    }
+};
