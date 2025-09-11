4f82bf48753548abbbdc26258651b51a72f5d893
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-09-11 10:18:05 +0530

    Time: 23 ms (20.56%) | Memory: 17 MB (25.20%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    unordered_set<char> vowels;
+public:
+    string sortVowels(string s) {
+        int n = s.size();
+        int counts[256] = {0};
+        
+        vowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+
+        for (char& c: s) {
+            if (vowels.count(c) > 0) {
+                counts[c]++;
+                c = '*';
+            }
+        }
+
+        int ptr = 'A';
+        for (char& c: s) {
+            if (c == '*') {
+                while (ptr < 256 && counts[ptr] == 0) ptr++;
+                c = (char)ptr;
+                counts[ptr]--;
+            }
+        }
+
+        return s;
+    }
+};
