052a0306199e31c2319024d0f497808af899c4ce
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-09-30 00:49:27 +0530

    Time: 3 ms (57.27%) | Memory: 11.1 MB (90.10%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
+        
         vector<vector<int>> dp(n, vector<int>(n, 0));
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
@@ -11,7 +12,7 @@ public:
                 }
             }
         }
-        
+
         return dp[0][n - 1];
     }
 };
