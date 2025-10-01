9a1bd1c79fe226aebd6bfed8e9e3020767df0f7a
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-10-01 13:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int result = 0,
+            empty = 0;
+
+        while (numBottles > 0) {
+            result += numBottles;
+            empty += numBottles;
+            numBottles = empty / numExchange;
+            empty = empty % numExchange;
+        }
+
+        return result;
+    }
+};
