9c6a707aa9c70e40b8a4719437897f286c7e33fa
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-10-02 12:40:20 +0530

    Time: 1 ms (30.32%) | Memory: 40.9 MB (57.42%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        for (int result = 0, empty = 0; numBottles >= 0;) {
+            result += numBottles;
+            empty += numBottles;
+            if (empty < numExchange) {
+                return result;
+            }
+
+            for (numBottles = 0; empty >= numExchange;) {
+                empty -= numExchange;
+                numBottles++;
+                numExchange++;
+            }
+        }
+
+        return 0;
+    }
+}
