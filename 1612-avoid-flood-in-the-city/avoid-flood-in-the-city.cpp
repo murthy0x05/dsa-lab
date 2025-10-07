81acb468db1be9af33ce3ad8c9e9ef3eee5d1199
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-10-07 19:47:40 +0530

    Time: 139 ms (29.66%) | Memory: 117.4 MB (20.06%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+
+        set<int> ids;
+        unordered_map<int, int> seen;
+        vector<int> result(n);
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                ids.insert(i);
+            } else {
+                if (seen.count(rains[i])) {
+                    if (ids.empty()) {
+                        return {};
+                    }  
+                    auto itr = ids.upper_bound(seen[rains[i]]);
+                    if (itr == ids.end()) {
+                        return {};
+                    }
+                    result[*itr] = rains[i];
+                    ids.erase(itr);
+                    seen.erase(rains[i]);
+                }
+                seen[rains[i]] = i; 
+                result[i] = -1;
+            }
+        }
+
+        cout << seen.count(2) << endl;
+
+        for (const int& id: ids) {
+            result[id] = 1;
+        }
+
+        return result;
+    }
+};
