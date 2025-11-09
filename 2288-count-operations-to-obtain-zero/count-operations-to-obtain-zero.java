5c9a9034abdb18b1eede5613378e4748b165a6ea
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-09 12:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (10.46%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -2,12 +2,9 @@ class Solution {
     public int countOperations(int num1, int num2) {
         int ops = 0;
         while (num1 != 0 && num2 != 0) {
-            ops++;
-            if (num1 > num2) {
-                num1 -= num2;
-            } else {
-                num2 -= num1;
-            }
+            ops += num1 / num2;
+            num1 = num1 % num2;
+            num1 = num2 ^ (num2 = num1) ^ num1;
         }
 
         return ops;
