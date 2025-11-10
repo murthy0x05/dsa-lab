1e14113762ab4e5d1dd24836da9d8541fddc21f8
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-11 01:21:49 +0530

    Time: 60 ms (37.93%) | Memory: 222 MB (78.51%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -2,17 +2,20 @@ class Solution {
     typedef pair<int, int> pii;
 public:
     int minOperations(vector<int>& nums) {
-        vector<int> stack(nums.size() + 1, 0);
-        int top = 0, ans = 0;
-        for (int num : nums) {
-            while (stack[top] > num) {
-                top--;
-                ans++;
+        stack<int> st; st.push(0);
+        int n = nums.size();
+
+        int ops = 0;
+        for (int i = 0; i < n; i++) {
+            while (st.top() > nums[i]) {
+                st.pop();
+                ops++;
             }
-            if (stack[top] != num)
-                stack[++top] = num;
+            if (st.top() != nums[i]) {
+                st.push(nums[i]);
+            }
         }
-        
-        return ans + top;
+
+        return st.size() + ops - 1;
     }
 };
