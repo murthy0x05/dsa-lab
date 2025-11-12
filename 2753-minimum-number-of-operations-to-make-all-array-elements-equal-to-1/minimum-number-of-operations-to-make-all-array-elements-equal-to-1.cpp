6c8ff11779a4aee9df2cc34bd5af8739e6d1bfd5
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-12 23:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (34.03%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -3,10 +3,13 @@ public:
     int minOperations(vector<int>& nums) {
         const int n = nums.size();
         int res = INT_MAX, cnt1 = 0;
+
         for (int i = 0; i < n; ++i)
             cnt1 += (nums[i] == 1);
+
         if (cnt1)
             return n - cnt1;
+            
         for (int i = 0; i < n; ++i) {
             int g = nums[i];
             for (int j = i + 1; j < n; ++j) {
@@ -17,6 +20,7 @@ public:
                 }
             }
         }
+
         return res == INT_MAX ? -1 : res;
     }
 };
