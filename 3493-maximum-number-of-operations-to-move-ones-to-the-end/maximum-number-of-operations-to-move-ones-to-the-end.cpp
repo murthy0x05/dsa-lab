4bcc0513f02b51d9c762bfd0b665e60e0ca0e09c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-13 18:19:55 +0530

    Time: 11 ms (14.13%) | Memory: 14.4 MB (95.65%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxOperations(const string& s) {
+    inline int maxOperations(const string& s) {
         int n = s.size();
 
         int ops = 0, po = s[0] - '0';
