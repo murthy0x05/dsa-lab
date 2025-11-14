baaedc4be209cd90a8651ca92152c9adf4a0124b
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-15 00:53:38 +0530

    Time: 24 ms (70.22%) | Memory: 93.3 MB (26.65%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
+        vector<vector<int>> df(n + 1, vector<int>(n + 1, 0));
 
-        // mark the boundaries of the 2d queries.
         for (auto& q : queries) {
             int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
-            diff[r1][c1]++;
-            diff[r2 + 1][c1]--;
-            diff[r1][c2 + 1]--;
-            diff[r2 + 1][c2 + 1]++;
+            df[r2 + 1][c1]--;
+            df[r1][c1]++;
+            df[r1][c2 + 1]--;
+            df[r2 + 1][c2 + 1]++;
         }
 
         vector<vector<int>> mat(n, vector<int>(n, 0));
@@ -18,8 +17,8 @@ public:
                 int above = i ? mat[i - 1][j] : 0;
                 int left = j ? mat[i][j - 1] : 0;
                 int diag = (i && j) ? mat[i - 1][j - 1] : 0;
-                
-                mat[i][j] = diff[i][j] + above + left - diag;
+
+                mat[i][j] = df[i][j] + left - diag + above;
             }
         }
 
