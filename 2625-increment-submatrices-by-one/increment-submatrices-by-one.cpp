95053ae3d3407cd5cc921e896e479b2e7e031e7e
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-15 00:12:19 +0530

    Time: 17 ms (79.94%) | Memory: 93.4 MB (15.67%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -12,7 +12,6 @@ public:
             diff[r2 + 1][c2 + 1]++;
         }
 
-        //reconstruct the restult
         vector<vector<int>> mat(n, vector<int>(n, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
