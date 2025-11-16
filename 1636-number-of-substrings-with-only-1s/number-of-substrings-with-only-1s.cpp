54d54b5efda8b23f0d815a69e8db9ead550c4a9e
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-16 12:14:24 +0530

    Time: 6 ms (9.77%) | Memory: 11.6 MB (39.33%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    const int MOD = 1e9 + 7;
+    using ll = long long;
+public:
+    int numSub(string s) {
+        int n = s.size();
+
+        int i = 0, j = 0;
+        ll result = 0;
+        while (i < n) {
+            if (s[i] == '0') {
+                i++; j++;
+            } else {
+                j++;
+                while (j < n && s[i] == s[j]) {
+                    j++;
+                }
+
+                result += ((j - i) * 1LL * (j - i + 1)) / 2;
+                i = j;
+            }
+        }
+
+        return result % MOD;
+    }
+};
