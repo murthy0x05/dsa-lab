3c51599db3357de335d9bf4e8cff7f811c983e6d
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-17 11:51:09 +0530

    Time: 1 ms (100.00%) | Memory: 61 MB (100.00%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.kt
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.kt
@@ -3,7 +3,7 @@ class Solution {
         val n: Int = nums.size
 
         var prev: Int = -100000
-        for (i in 0 .. n-1) {
+        for (i in 0 until n) {
             if (nums[i] == 1) {
                 if (i - prev - 1 < k) {
                     return false
