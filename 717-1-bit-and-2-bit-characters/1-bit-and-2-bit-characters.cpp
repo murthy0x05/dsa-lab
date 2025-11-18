1da464fc8dc3b7ff9835ae33bb31105b63c35e51
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-11-18 11:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (72.06%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -6,8 +6,8 @@ public:
         for (int i = 0; i < n; i++) {
             if (bits[i] == 1) {
                 i++;
-            } else {
-                if (i == n - 1) return true;
+            } else if (i == n - 1) {
+                return true;
             }
         }
 
