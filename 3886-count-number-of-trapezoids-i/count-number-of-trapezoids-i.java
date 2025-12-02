bc0c5e57ecb84383f45a06976c2be7b32f6122d1
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-12-02 12:37:47 +0530

    Time: 66 ms (28.57%) | Memory: 159.6 MB (35.20%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    private final int MOD = (int)1e9 + 7;
+    public int countTrapezoids(int[][] points) {
+        Arrays.sort(points, (a, b) -> { return a[1] - b[1]; });
+
+        int n = points.length;
+        int sum = 0,
+            result = 0;
+        for (int i = 1, s; i < n; i++) {
+            s = 1;
+            while (i < n && points[i][1] == points[i - 1][1]) {
+                i++;
+                s++;
+            }
+
+            if (s >= 2) {
+                int sC2 = (int)(((s * 1L * (s - 1)) / 2) % MOD);
+                result = (int)((result + (sum * 1L * sC2)) % MOD);
+                sum = (int)((sum + sC2) % MOD);
+            }
+        }
+
+        return result;
+    }
+}
