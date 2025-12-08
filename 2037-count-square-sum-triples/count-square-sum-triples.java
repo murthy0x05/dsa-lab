32155c2016b1a8add680ecbfef079e86182c74eb
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-12-08 20:00:51 +0530

    Time: 3 ms (97.70%) | Memory: 41.7 MB (66.18%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countTriples(int n) {
+        int result = 0;
+        for (int c = 3; c <= n; c++) {
+            int l = 1, r = c - 1;
+            
+            int needed = c * c;
+            while (l < r) {
+                int sum = l * l + r * r;
+                if (needed == sum) {
+                    result += 2;
+                    l++; r--;
+                } else if (sum < needed) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return result;   
+    }
+}
