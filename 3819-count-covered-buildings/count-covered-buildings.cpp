8120fa160856bd063cc33bdcd7e4c7cf94277e43
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-12-11 15:18:10 +0530

    Time: 155 ms (77.90%) | Memory: 278.7 MB (99.45%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        sort(buildings.begin(), buildings.end(), [](vector<int>& a, vector<int>& b) {
+            if (a[0] != b[0]) {
+                return a[0] < b[0];
+            } else {
+                return a[1] < b[1];
+            }
+        });
+
+        int i = 1,
+            sz = buildings.size(),
+            len = 1;
+        vector<pair<int, int>> y(n + 1, make_pair(INT_MAX, INT_MIN));
+        for (int i = 0; i < sz; i++) {
+            y[buildings[i][1]].first = min(y[buildings[i][1]].first, buildings[i][0]);
+            y[buildings[i][1]].second = max(y[buildings[i][1]].second, buildings[i][0]);
+        }
+
+        int result = 0;
+        for (int i = 1; i < sz - 1; i++) {
+            if (buildings[i][0] == buildings[i - 1][0] &&
+                buildings[i][0] == buildings[i + 1][0] &&
+                buildings[i][0] > y[buildings[i][1]].first &&
+                buildings[i][0] < y[buildings[i][1]].second) {
+                result++;
+            }
+        }
+
+        return result;
+
+    }
+};
