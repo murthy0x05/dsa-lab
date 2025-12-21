ecedffc6272ea3ba47522edcde3ff8eacb09bad8
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-12-21 17:23:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (38.20%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int r = strs.size(),
-            c = strs[0].size();
+        int count=0;
+        vector<bool> sorted(strs.size() - 1, false);
 
-        unordered_set<int> deleted;
-        vector<vector<int>> stoppedAt;
-        function<int(int, int, int)> dfs = [&](int col, int rs, int re) {
-            if (col >= c) {
-                return 0;
-            }
-
-            int len = 1;
-            vector<pair<int, int>> blocks;
-            for (int i = rs + 1; i <= re; i++) {
-                if (strs[i - 1][col] != strs[i][col]) {
-                    if (strs[i - 1][col] > strs[i][col]) {
-                        deleted.insert(col);
-                        dfs(col + 1, rs, re);
-                        return (int)deleted.size();
-                    } else {
-                        if (len > 1) {
-                            blocks.push_back(make_pair(i - len, i - 1));
-                        }
-                        len = 1;
-                    }
-                } else {
-                    len++;
+        for(int i=0;i<strs[0].size();i++){
+            bool check=true;
+            
+            for(int j=0;j<strs.size()-1;j++){
+                if(!sorted[j] && strs[j][i]>strs[j+1][i]){
+                    check=false;
+                    break;
                 }
             }
-            if (len > 1) {
-                blocks.push_back(make_pair(re - len + 1, re));
-            }
-            stoppedAt.push_back({col, rs, re});
+
+            if(check==false){
+                count++;
+                continue;
+            } 
             
-
-            for (pair<int, int>& block: blocks) {
-                dfs(col + 1, block.first, block.second);
+            for(int j=0;j<strs.size()-1;j++){
+                if(!sorted[j] && strs[j][i]<strs[j+1][i]){
+                    sorted[j]=true;
+                }
             }
-
-            return (int)deleted.size();
-        };
-        
-        dfs(0, 0, r - 1);
-
-size_t n = stoppedAt.size();
-for (size_t i = 0; i < n; ++i) {
-    auto &at = stoppedAt[i];
-    if (deleted.count(at[0])) {
-        dfs(at[0] + 1, at[1], at[2]);
-    }
-}
-
-
-        return deleted.size();
+        }
+        return count;
     }
 };
