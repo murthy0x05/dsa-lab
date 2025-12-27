786fd885bb6bfd9f5e55fed497f2c2940cff3ab3
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2025-12-27 23:22:40 +0530

    Time: 59 ms (95.65%) | Memory: 98.1 MB (88.93%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,19 +1,8 @@
 class Solution {
-    typedef long long ll;
-    typedef pair<int, ll> pill;
-    struct Comparator {
-        bool operator() (auto& a, auto& b) {
-            if (a.second != b.second)
-                return a.second > b.second;
-            else
-                return a.first > b.first;
-        }
-    };
+    using ll = long long;
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        int m = meetings.size();
-        vector<int> bookings(n, 0);
-        sort(meetings.begin(), meetings.end(), [](auto& a, auto& b) {
+        sort(meetings.begin(), meetings.end(), [](vector<int>& a, vector<int>& b) {
             if (a[0] != b[0]) {
                 return a[0] < b[0];
             } else {
@@ -21,41 +10,41 @@ public:
             }
         });
 
-        priority_queue<int, vector<int>, greater<int>> available;
-        priority_queue<pill, vector<pill>, Comparator> occupied;
-        for (int i = 0; i < n; i++)
-            available.push(i);
-
-        for (int i = 0; i < m; i++) {
-            while (!occupied.empty() && occupied.top().second <= meetings[i][0]) {
-                available.push(occupied.top().first);
-                occupied.pop();
-            }
-            if (available.empty()) {
-                pair<int, ll> room = occupied.top();
-                occupied.pop();
-                occupied.push(make_pair(room.first, room.second + (meetings[i][1] - meetings[i][0])));
-                bookings[room.first] += 1;
+        priority_queue<int, vector<int>, greater<>> unused;
+        vector<ll> lru(n, -1);
+        auto cmp = [&lru] (const int& a, const int& b) {
+            if (lru[a] != lru[b]) {
+                return lru[a] > lru[b];
             } else {
-                int room = available.top();
-                available.pop();
-                occupied.push(make_pair(room, meetings[i][1]));
-                bookings[room] += 1;
+                return a > b;
+            }
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> used(cmp);
+        vector<int> booked(n, 0);
+        
+        booked[0]++;
+        lru[0] = meetings[0][1];
+        used.push(0);
+        
+        for (int i = 1; i < n; i++) unused.push(i);
+        for (int i = 1, len = meetings.size(); i < len; i++) {
+            while (!used.empty() && lru[used.top()] <= meetings[i][0]) {
+                unused.push(used.top()); used.pop();
+            }
+            
+            if (!unused.empty()) {
+                booked[unused.top()]++;
+                lru[unused.top()] = meetings[i][1];
+                used.push(unused.top());
+                unused.pop();
+            } else {
+                int room = used.top(); used.pop();
+                booked[room]++;
+                lru[room] += (meetings[i][1] - meetings[i][0]);
+                used.push(room);
             }
         }
 
-        // for (int i: bookings) {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-
-        int most_booked = 0;
-        for (int i = 1; i < n; i++) {
-            if (bookings[i] > bookings[most_booked]) {
-                most_booked = i;
-            }
-        }
-
-        return most_booked;
+        return max_element(booked.begin(), booked.end()) - booked.begin();
     }
 };
