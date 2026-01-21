a2b6d348d7ac7f672d7ebbd4cf196831371124e2
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-21 23:26:24 +0530

    Time: 1 ms (2.98%) | Memory: 11.9 MB (5.83%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        function<pair<TreeNode*, int>(TreeNode*, int)> dfs = [&](TreeNode* bst, int val) {
+            if (!bst) {
+                return make_pair((TreeNode*)nullptr, 0);
+            }
+
+            TreeNode* gst = new TreeNode(bst -> val);
+
+            pair<TreeNode*, int> right = dfs(bst -> right, val);
+            gst -> right = right.first;
+            gst -> val += right.second;
+            gst -> val += val;
+
+            pair<TreeNode*, int> left = dfs(bst -> left, gst -> val);
+            gst -> left = left.first;
+
+            return make_pair(gst, bst -> val + left.second + right.second);
+        };
+
+        return dfs(root, 0).first;
+    }
+};
