6252d7e446c89ab865a7b9d9b4384b778770121c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-22 09:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (53.72%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        int left = 0;
+        for (int i = 0, n = nums.size(); i < n; i++) {
+            if (left == right - nums[i]) {
+                return i;
+            } else {
+                left += nums[i];
+                right -= nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
