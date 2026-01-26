793bf1502dbd4b4956e2672a98eb61ca2f2c6fc3
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-26 13:32:50 +0530

    Time: 18 ms (30.37%) | Memory: 41.6 MB (5.04%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -5,24 +5,16 @@ public:
         sort(arr.begin(), arr.end());
 
         int abs_diff = arr.back() - arr.front() + 1;
-        vector<vector<int>> result;
+        vector<vector<int>> result; result.reserve(n - 1);
+        result.assign(n - 1, vector<int>(0));
         int len = 0;
         for (int i = 1; i < n; i++) {
             if (arr[i] - arr[i - 1] == abs_diff) {
-                if (result.size() <= len) {
-                    result.push_back({arr[i - 1], arr[i]});
-                } else {
-                    result[len] = {arr[i - 1], arr[i]};
-                }
-                len++;
+                result[len++] = {arr[i - 1], arr[i]};
             } else if (arr[i] - arr[i - 1] < abs_diff) {
                 abs_diff = arr[i] - arr[i - 1];
                 len = 0;
-                if (result.size() <= len) {
-                    result.push_back({arr[i - 1], arr[i]});
-                } else {
-                    result[len] = {arr[i - 1], arr[i]};
-                }
+                result[len] = {arr[i - 1], arr[i]};
                 len++;
             }
         }
