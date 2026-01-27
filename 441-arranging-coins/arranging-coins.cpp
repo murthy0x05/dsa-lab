7baba57a9a40d218b33f140af18faf11ca6bf24f
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-27 15:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (34.92%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -3,12 +3,10 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         ll low = 1, high = n;
-        int result = -1;
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
             ll X = (mid * 1LL * (mid + 1)) / 2;
             if (X <= n) {
-                result = low;
                 low = mid + 1;
             } else {
                 high = mid - 1;
