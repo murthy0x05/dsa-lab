7262c2bc278d55cbbfd2ca5b115b0713c8f00bf8
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-27 23:31:37 +0530

    Time: 309 ms (5.02%) | Memory: 77.1 MB (94.63%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> counts(1e5 + 1, 0);
+        for (int i = 0; i < n; i++) {
+            counts[arr[i]]++;
+        }
+
+        sort(counts.rbegin(), counts.rend());
+        int result = 0;
+        for (int i = 0, removed = 0; removed < (n / 2); i++) {
+            removed += counts[i];
+            result++;
+        }
+
+        return result;
+    }
+};
