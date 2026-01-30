0aa2b425726f00a4799e77d8fd522c35f56a62ae
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-01-31 02:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (26.56%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,42 +1,35 @@
 class Solution {
-    bool can_split(vector<int>& nums, int total, int k) {
-        int i = 0, n = nums.size(), curr_sum = 0, splits = 0;
-        if (*max_element(nums.begin(), nums.end()) > total)
-            return false;
-        while (i < n) {
-            if (curr_sum + nums[i] > total) {
-                curr_sum = nums[i];
-                splits++;
-            } else {
-                curr_sum += nums[i];
-            }
-            i++;
-        }
-
-        return splits + 1 <= k;
-    }
 public:
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-        int sum = 0, largest_sum = 0;
-        for (int& num: nums) {
-            sum += num;
-            largest_sum = max(largest_sum, num);
-        }
-        // cout << sum << endl;
 
-        int low = 0, high = sum;
+        function<bool(int&)> feasible = [&](int& total) {
+            int K = 1;
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (sum + nums[i] > total) {
+                    sum = 0;
+                    K++;
+                }
+                sum += nums[i];
+            }
+
+            return K <= k;
+        };
+
+        int low = *max_element(nums.begin(), nums.end()),
+            high = accumulate(nums.begin(), nums.end(), 0);
+        int result = 0;
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if (can_split(nums, mid, k)) {
-                // cout << mid << endl;
-                largest_sum = mid;
+            if (feasible(mid)) {
+                result = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
 
-        return largest_sum;
+        return result;
     }
 };
