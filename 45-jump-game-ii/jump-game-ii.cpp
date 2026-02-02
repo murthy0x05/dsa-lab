76aa46983daa7ff90dc581fb4335f333e907608d
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-03 00:17:50 +0530

    Time: 4 ms (28.87%) | Memory: 22 MB (10.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,13 +3,10 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         const int n = nums.size();
-        if (n == 1) {
-            return 0;
-        }
         
-        vector<int> dp(n); dp[0] = 1;
+        vector<int> dp(n); dp[0] = 0;
         priority_queue<pii, vector<pii>, greater<pii>> pq;
-        pq.push(make_pair(1, 0 + nums[0]));
+        pq.push(make_pair(0, 0 + nums[0]));
 
         for (int i = 1; i < n; i++) {
             while (pq.top().second < i) {
@@ -21,6 +18,6 @@ public:
         }
 
 
-        return dp.back();
+        return dp.back() + (n != 1);
     }
 };
