cdb34e03ef089c565cae68664a35ab866c3fdc80
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-04 02:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (54.18%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -1,36 +1,24 @@
 class Solution {
+    using ll = long long;
 public:
     int countDigitOne(int n) {
-        if (n == 0) return 0;
-        string N = to_string(n);
-
-        vector<int> ones(11);
-        ones[0] = 0;
-
-        int total = 0;
-        for (int size = 1, len = N.size(); size < len; size++) {
-            ones[size] = (ones[size - 1] * 9) + ((int)pow(10, size - 1));
-            ones[size] += ones[size - 1];
-        }
+        int result = 0;
+        for (ll place = 1; place <= n; place *= 10LL) {
+            int higher = n / place;
+            int current = higher % 10;
+            int lower = n % place;
+            higher /= 10;
 
 
-        int len = N.size();
-        int oneCount = N.back() >= '1';
-        for (int i = len - 2; i >= 0; i--) {
-            if (i == 0) 
-                oneCount += (N[i] - '1') * ones[len - i - 1];
-            else
-                oneCount += (N[i] - '0') * ones[len - i - 1];
-            if (N[i] >= '1') {
-                if (N[i] > '1') {
-                    oneCount += pow(10, len - i - 1);
-                } else {
-                    oneCount += (stoi(N.substr(i + 1)) + 1);
-                }
+            if (current == 0) {
+                result += higher * place;
+            } else if (current == 1) {
+                result += (higher * place) + (lower + 1);
+            } else {
+                result += (higher + 1) * place;
             }
         }
-        ones[len] = oneCount;
 
-        return oneCount + ones[len - 1];
+        return result;
     }
 };
