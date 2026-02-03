ac5dcad8aa5ea7fc990a930c9f6ae06bbbe9b076
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-03 12:44:47 +0530

    Time: 19 ms (56.70%) | Memory: 169.7 MB (90.07%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
@@ -3,19 +3,12 @@ public:
     int climbStairs(int n, vector<int>& costs) {
         const int INF = 1e9 + 10;
 
-        vector<int> dp(n + 1, INF);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = min(dp[i - 1] + 1, dp[i]);
-            if (i >= 2) {
-                dp[i] = min(dp[i - 2] + 4, dp[i]);
-            }
-            if (i >= 3) {
-                dp[i] = min(dp[i - 3] + 9, dp[i]);
-            }
-            dp[i] += costs[i - 1];
+        int p1 = 0, p2 = INF, p3 = INF;
+        for (int i = 1, curr; i <= n; i++) {
+            curr = min({p1 + 1, p2 + 4, p3 + 9}) + costs[i - 1];
+            p3 = p2; p2 = p1; p1 = curr;
         }
 
-        return dp.back();
+        return p1;
     }
 };
