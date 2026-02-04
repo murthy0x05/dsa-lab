54fc22c06775a9fc51eef3be1d2293fceaf13e28
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-04 11:31:52 +0530

    Time: 11 ms (42.11%) | Memory: 9.4 MB (46.86%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -8,7 +8,7 @@ public:
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i / 2; j++) {
                 if (i % j == 0) {
-                    dp[i] = min(dp[i], dp[j] + 1 + (i/j - 1));
+                    dp[i] = min(dp[i], dp[j] + 1 + ((i/j) - 1));
                 }
             }
         }
