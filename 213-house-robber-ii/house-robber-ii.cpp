baac0639ed71a58653d6d476087daef322a8888e
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-05 00:16:32 +0530

    Time: 4 ms (1.21%) | Memory: 10.8 MB (38.75%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -11,16 +11,15 @@ public:
         vector<int> dp(n);
         dp[0] = nums[0]; dp[1] = nums[1]; dp[2] = nums[2] + dp[0];
         for (int i = 3; i < n - 1; i++) {
-            dp[i] = max(dp[i - 1], nums[i] + max(dp[i - 2], dp[i - 3]));
+            dp[i] = nums[i] + max(dp[i - 2], dp[i - 3]);
         }
-
-        int result = dp[n - 2];
+        int result = max(dp[n - 3], dp[n - 2]);
 
         dp[0] = 0; dp[1] = nums[1]; dp[2] = nums[2];
         for (int i = 3; i < n; i++) {
-            dp[i] = max(dp[i - 1], nums[i] + max(dp[i - 2], dp[i - 3]));
+            dp[i] = nums[i] + max(dp[i - 2], dp[i - 3]);
         }
 
-        return max(result, dp[n - 1]);
+        return max(result, max(dp[n - 2], dp[n - 1]));
     }
 };
