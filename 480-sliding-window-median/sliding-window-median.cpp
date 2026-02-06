a3eef90351bca7009562ec845364f4ab2bb53538
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-07 00:40:33 +0530

    Time: 328 ms (5.02%) | Memory: 49.7 MB (43.36%) - LeetSync

---
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        const int N = nums.size();
+
+        vector<double> result; result.reserve(N);
+        multiset<int> left, right;
+
+        for (int i = 0; i < N; i++) {
+            if (i < (k - 1)) {
+                if (left.empty()) {
+                    left.insert(nums[i]);
+                } else {
+                    if (*prev(left.end()) >= nums[i]) {
+                        left.insert(nums[i]);
+                    } else {
+                        right.insert(nums[i]);
+                    }
+
+                    if (abs<int>(left.size() - right.size()) > 1) {
+                        if (left.size() > right.size()) {
+                            right.insert(*prev(left.end()));
+                            left.erase(prev(left.end()));
+                        } else {
+                            left.insert(*right.begin());
+                            right.erase(right.begin());
+                        }
+                    }
+                }
+            } else {
+                if (left.empty()) {
+                    left.insert(nums[i]);
+                } else {
+                    if (*prev(left.end()) >= nums[i]) {
+                        left.insert(nums[i]);
+                    } else {
+                        right.insert(nums[i]);
+                    } 
+                }
+
+
+                if (abs<int>(left.size() - right.size()) > 1) {
+                    if (left.size() > right.size()) {
+                        right.insert(*prev(left.end()));
+                        left.erase(prev(left.end()));
+                    } else {
+                        left.insert(*right.begin());
+                        right.erase(right.begin());
+                    }
+                }
+
+                if (left.size() == right.size()) {
+                    cout << *prev(left.end()) << ' ' << *right.begin() << endl;
+                    result.push_back((0LL + *prev(left.end()) + *right.begin()) / 2.0);
+                } else if (left.size() > right.size()) {
+                    result.push_back(*prev(left.end()));
+                } else {
+                    result.push_back(*right.begin());
+                }
+
+                if (left.find(nums[i - k + 1]) != left.end()) {
+                    left.erase(left.find(nums[i - k + 1]));
+                } else {
+                    right.erase(right.find(nums[i - k + 1]));
+                }
+                
+                if (abs<int>(left.size() - right.size()) > 1) {
+                    if (left.size() > right.size()) {
+                        right.insert(*prev(left.end()));
+                        left.erase(prev(left.end()));
+                    } else {
+                        left.insert(*right.begin());
+                        right.erase(right.begin());
+                    }
+                }
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+        return result;
+    }
+};
