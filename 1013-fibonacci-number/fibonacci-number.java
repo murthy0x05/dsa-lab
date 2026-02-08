b2019d32b97bb7074f77276a6fb9323895edf215
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-09 00:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (48.33%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 2) return n;
+
+        int[] F = new int[31];
+        F[0] = 0; F[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            F[i] = F[i - 1] + F[i - 2];
+        }
+
+        return F[n];
+    }
+}
