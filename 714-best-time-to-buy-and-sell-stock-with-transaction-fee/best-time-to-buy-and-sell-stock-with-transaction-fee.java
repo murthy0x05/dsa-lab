164d443f1ec362398d7bfed43b90562b4de778f2
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-09 01:42:20 +0530

    Time: 4 ms (99.53%) | Memory: 65.5 MB (68.11%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -5,7 +5,7 @@ class Solution {
 
         int prev = prices[0], profit = 0;
         for (int i = 1; i < N; i++) {
-            if ((prices[i] - prev - fee) > 0) {
+            if ((prices[i] - prev - fee) >= 0) {
                 profit += prices[i] - prev - fee;
                 prev = prices[i] - fee;
             } else {
