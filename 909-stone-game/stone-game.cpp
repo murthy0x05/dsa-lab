659d3159b59df7273e91d4a2851668cc2df1907c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-09 19:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.74%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        long long x = 0;
-        for (long long i = 0; i < 10000000000; i++) {
-            x++;
-        }
         return true;
     }
 };
