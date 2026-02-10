5508f29fd8411ad8be09ce88733791a4ca63af57
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-10 23:36:11 +0530

    Time: 1974 ms (38.37%) | Memory: 527.5 MB (47.80%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -7,15 +7,15 @@ public:
         for (int i = 1; i < N; i++) {
             unordered_set<int> seen = {nums[i]};
             int even = !(nums[i] & 1), odd = (nums[i] & 1);
+
             for (int j = i - 1; j >= 0; j--) {
                 if (!seen.count(nums[j])) {
                     even += !(nums[j] & 1);
                     odd += (nums[j] & 1);
                     seen.insert(nums[j]);
                 }
-                if (even == odd) {
+                if (even == odd)
                     result = max(result, i - j + 1);
-                }
             }
         }
 
