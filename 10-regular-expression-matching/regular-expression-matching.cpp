03bbb5a950f3a44eb93a4a1b9a45c8991f5d7069
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-12 15:36:15 +0530

    Time: 2 ms (59.20%) | Memory: 10.5 MB (11.92%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(),
+            m = p.size();
+
+        vector<vector<char>> mem(n, vector<char>(m, '!'));
+        function<bool(int, int)> f = [&] (int n, int m) {
+            if (m < 0) {
+                return n < 0;
+            } else if (n < 0) {
+                while (m >= 0) {
+                    if (p[m] != '*') {
+                        return false;
+                    }
+                    m -= 2;
+                }
+
+                return true;
+            } else {
+                if (mem[n][m] != '!')
+                    return mem[n][m] == 'y';
+
+                bool matched = false;
+                if (p[m] == '.' || s[n] == p[m]) {
+                    matched = f(n - 1, m - 1);
+                } else if (p[m] == '*') {
+                    matched = f(n, m - 2);
+                    if (s[n] == p[m - 1] || p[m - 1] == '.') {
+                        matched = matched || f(n - 1, m);
+                    }
+                }
+
+                mem[n][m] = matched ? 'y' : 'n';
+                return matched;
+            }
+        }; 
+
+        return f(n - 1, m - 1);
+    }
+};
