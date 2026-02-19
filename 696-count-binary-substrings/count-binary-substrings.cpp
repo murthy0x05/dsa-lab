83f887497b3dc39eee577623764fd88f2b1d4772
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-02-19 09:35:08 +0530

    Time: 4 ms (52.74%) | Memory: 13.1 MB (32.17%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -4,8 +4,7 @@ public:
         const int N = s.size();
         
         int result = 0;
-        for (int l = 0, r = 0, po = 0, st; r < N; po = l, l = r) {
-            st = l;
+        for (int l = 0, r = 0, po = 0, st; ((st = l) || 1) && r < N; po = l, l = r) {
             while (l < N && s[l] == '0') {
                 l++;
             }
