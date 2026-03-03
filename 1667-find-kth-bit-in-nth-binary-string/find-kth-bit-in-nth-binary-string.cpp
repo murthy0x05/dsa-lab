cab8cb36496e3b5eaa9f76be7680a293863de0fe
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-03-03 19:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.29%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        vector<int> sz(21, 0);
+        sz[1] = 1;
+        for (int i = 2; i <= 20; i++) {
+            sz[i] = (sz[i - 1] * 2) + 1;
+        }
+
+        function<bool(int, int)> f = [&] (int N, int K) {
+            if (N == 1) {
+                return false;
+            }
+
+            int mid = sz[N] / 2;
+            if (K == mid) {
+                return true;
+            } else if (K < mid) {
+                return f(N - 1, K);
+            } else {
+                return !f(N - 1, 2 * mid - K);
+            }
+        };
+
+        return f(n, k - 1) ? '1' : '0';
+    }
+};
