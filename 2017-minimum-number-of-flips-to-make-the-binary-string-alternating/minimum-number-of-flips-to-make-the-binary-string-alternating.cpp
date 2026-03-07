bd5b1624e7e6073ba8fd614a39617b9e238e2fab
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-03-08 00:53:50 +0530

    Time: 19 ms (57.20%) | Memory: 14.5 MB (76.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,41 +3,32 @@ public:
     int minFlips(string s) {
         const int N = s.size();
 
-        bool curr1 = false,
-             curr2 = true;
-        int ops1 = 0,
-            ops2 = 0;
+        int ops1 = 0, ops2 = 0;
         for (int i = 0; i < N; i++) {
-            bool curr = (s[i] == '1');
-            if (curr1 != curr) {
+            if ((i & 1) != (s[i] == '1')) {
                 ops1++;
             }
-            if (curr2 != curr) {
+            if ((i & 1 ^ 1) != (s[i] == '1')) {
                 ops2++;
             }
-            curr1 = !curr1;
-            curr2 = !curr2;
         }
 
         int least = min(ops1, ops2);
-        for (int i = 0; i < N; i++) {
-            bool prefix = s[i] == '1';
-            if (curr1 != prefix) {
-                ops1++;
-            }
-            if (curr2 != prefix) {
-                ops2++;
-            }
-            if ((i & 1) != prefix) {
+        for (int i = 0; i < N - 1; i++) {
+            if ((i & 1) != (s[i] == '1')) {
                 ops1--;
             }
-            if ((!(i & 1)) != prefix) {
+            if ((1 ^ i & 1) != (s[i] == '1')) {
                 ops2--;
             }
-
+            if (((N + i) & 1) != (s[i] == '1')) {
+                ops1++;
+            }
+            if (((N + i) & 1 ^ 1) != (s[i] == '1')) {
+                ops2++;
+            }
+            
             least = min({least, ops1, ops2});
-            curr1 = !curr1;
-            curr2 = !curr2;
         }
 
         return least;
