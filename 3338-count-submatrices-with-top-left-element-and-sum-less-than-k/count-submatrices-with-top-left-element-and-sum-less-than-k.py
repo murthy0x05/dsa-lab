68a8a1583cab4be167a21f3b5a8b0c054286e834
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-03-18 15:57:59 +0530

    Time: 335 ms (51.72%) | Memory: 67.2 MB (31.03%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -2,14 +2,11 @@ class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
         R, C = len(grid), len(grid[0])
 
+        count = 0
         ps = [[0 for _ in range(C + 1)] for _ in range(R + 1)]
         for i in range(R):
             for j in range(C):
                 ps[i + 1][j + 1] = ps[i][j + 1] + ps[i + 1][j] - ps[i][j] + grid[i][j]
-
-        count = 0
-        for i in range(R):
-            for j in range(C):
                 count += 1 if ps[i + 1][j + 1] <= k else 0
-        
+
         return count
