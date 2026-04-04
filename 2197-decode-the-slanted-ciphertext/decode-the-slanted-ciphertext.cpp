e71e1a5bbb428a2faba596633275fd2580c066fe
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-04-04 20:20:43 +0530

    Time: 35 ms (51.69%) | Memory: 41.8 MB (63.56%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        const int N = encodedText.size();
+
+        string result;
+        int cols = N / rows;
+        for (int k = 0; k < cols; k++) {
+            for (int i = 0, j = k; i < rows && j < cols; i++, j++) {
+                result.push_back(encodedText[i * cols + j]);
+            }
+        }
+        while (!result.empty() && isspace(result.back())) {
+            result.pop_back();
+        }
+
+
+        return result;
+    }
+};
