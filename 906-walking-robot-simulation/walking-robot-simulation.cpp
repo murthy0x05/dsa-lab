97fc7907587db3cb644f92e972f3e344e4d80368
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-04-06 19:45:05 +0530

    Time: 184 ms (6.13%) | Memory: 53.3 MB (10.40%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        const int N = commands.size();
+
+        unordered_map<int, unordered_set<int>> obs;
+        for (const vector<int>& obstacle: obstacles) {
+            obs[obstacle[0]].insert(obstacle[1]);
+        }
+
+        int x = 0, y = 0;
+        int dx = 0, dy = 1;
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (commands[i] > 0) {
+                for (int j = 0; j < commands[i]; j++) {
+                    if (obs.count(x + dx) && obs[x + dx].count(y + dy)) {
+                        break;
+                    } else {
+                        x += dx;
+                        y += dy;
+                    }
+                }
+                cout << x << ' ' << y << endl;
+            } else if (commands[i] == -2) {
+                if (dx == 0 && dy == 1) {
+                    dx = -1;
+                    dy = 0;
+                } else if (dx == -1 && dy == 0) {
+                    dx = 0;
+                    dy = -1;
+                } else if (dx == 0 && dy == -1) {
+                    dx = 1;
+                    dy = 0;
+                } else {
+                    dx = 0;
+                    dy = 1;
+                }
+            } else {
+                if (dx == 0 && dy == 1) {
+                    dx = 1;
+                    dy = 0;
+                } else if (dx == 1 && dy == 0) {
+                    dx = 0;
+                    dy = -1;
+                } else if (dx == 0 && dy == -1) {
+                    dx = -1;
+                    dy = 0;
+                } else {
+                    dx = 0;
+                    dy = 1;
+                }
+            }
+
+
+            result = max(result, x * x + y * y);
+        }
+
+        return result;
+    }
+};
