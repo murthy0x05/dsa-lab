fbc6f9b490350e86034d08a57b6a1a5e8871f99a
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-06-26 10:44:02 +0530

    Time: 1487 ms (5.10%) | Memory: 46.2 MB (86.13%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -1,26 +1,34 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        int N1 = nums1.length,
-            N2 = nums2.length;
+        final int N1 = nums1.length,
+                  N2 = nums2.length;
         if (N1 > N2) {
             return findLength(nums2, nums1);
         }
-        
-        int maxLength = 0;
-        int[] prev = new int[N2 + 1];
-        for (int i = 0; i < N1; i++) {
-            int[] curr = new int[N2 + 1];
-            for (int j = 0; j < N2; j++) {
-                if (nums1[i] == nums2[j]) {
-                    curr[j + 1] = prev[j] + 1;
-                    maxLength = Math.max(maxLength, curr[j + 1]);
-                } else {
-                    curr[j + 1] = 0;
-                }
-            }
-            prev = curr;
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        return maxLength;
+        for (int i = 0; i < N1; i++) {
+            adj.get(nums1[i]).add(i);
+        }
+
+        int longest = 0;
+        for (int i = 0; i < N2; i++) {
+            for (int id: adj.get(nums2[i])) {
+                int c1id = id;
+                int c2id = i;
+                while (c1id < N1 && c2id < N2 && nums1[c1id] == nums2[c2id]) {
+                    c1id++;
+                    c2id++;
+                }
+
+                longest = Math.max(longest, c1id - id);
+            }
+        }
+
+        return longest;
     }
 }
