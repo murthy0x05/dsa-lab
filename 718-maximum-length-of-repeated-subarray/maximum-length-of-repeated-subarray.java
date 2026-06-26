7bcd0c3f6be736a0057407faabca1449c6905a26
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-06-26 10:47:35 +0530

    Time: 1483 ms (5.10%) | Memory: 46.1 MB (86.40%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -6,18 +6,18 @@ class Solution {
             return findLength(nums2, nums1);
         }
 
-        List<List<Integer>> adj = new ArrayList<>();
+        List<Integer>[] adj = new ArrayList[101];
         for (int i = 0; i < 101; i++) {
-            adj.add(new ArrayList<>());
+            adj[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < N1; i++) {
-            adj.get(nums1[i]).add(i);
+            adj[nums1[i]].add(i);
         }
 
         int longest = 0;
         for (int i = 0; i < N2; i++) {
-            for (int id: adj.get(nums2[i])) {
+            for (int id: adj[nums2[i]]) {
                 int c1id = id;
                 int c2id = i;
                 while (c1id < N1 && c2id < N2 && nums1[c1id] == nums2[c2id]) {
