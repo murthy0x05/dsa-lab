84811865b6decf5bbf0cef4ad379ba86d22b41d5
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-07-09 22:45:20 +0530

    Time: 99 ms (58.73%) | Memory: 49.4 MB (80.16%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -2,11 +2,9 @@ class Solution:
     def pathExistenceQueries(self, V: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
         exts = [0 for _ in range(V)]
         
-        for i in range(1, V):
-            exts[i] = exts[i - 1] if nums[i] - nums[i - 1] <= maxDiff else i
+        for i in range(1, V): exts[i] = exts[i - 1] if nums[i] - nums[i - 1] <= maxDiff else i
         
         result = []
-        for q in queries:
-            result.append(exts[q[1]] <= q[0] if q[0] < q[1] else exts[q[0]] <= q[1])
+        for q in queries: result.append(exts[q[1]] <= q[0] if q[0] < q[1] else exts[q[0]] <= q[1])
     
         return result
