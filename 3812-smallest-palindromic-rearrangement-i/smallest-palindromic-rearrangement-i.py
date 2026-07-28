7105c71898bb531fdb16859ee9b34b8f05fd5543
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-07-28 16:19:35 +0530

    Time: 648 ms (8.30%) | Memory: 21.5 MB (18.43%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        N = len(s)
+
+        freq = [0] * 26
+        for c in s:
+            freq[ord(c) - ord('a')] += 1
+
+        id = 0
+        smallest = [''] * N
+        for i in range(26):
+            if freq[i] & 1:
+                smallest[N // 2] = chr(ord('a') + i)
+                freq[i] -= 1
+
+            while freq[i] > 0:
+                smallest[id] = smallest[N - id - 1] = chr(ord('a') + i)
+                freq[i] -= 2
+                id += 1
+
+
+        
+        return ''.join(smallest)
