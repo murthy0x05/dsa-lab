b96154a62d7b4efde4bf9eaf9675f18d4618cdfc
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-08-09 12:59:02 +0530

    Time: 354 ms (5.01%) | Memory: 243.8 MB (5.03%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,33 +1,33 @@
 class Solution {
-    int f (vector<pair<int, int>>& mem, const int& N, const vector<int>& stoneValue, int i, bool alice) {
-        if (i >= N) {
-            return 0;
-        }
-
-        if ((alice and mem[i].first != 1e9) or (!alice and mem[i].second != 1e9)) {
-            return alice ? mem[i].first : mem[i].second;
-        }
-        
-        int procure = alice ? INT_MIN : INT_MAX;
-        int got = 0;
-        for (int X = 0; X < 3 and i + X < N; X++) {
-            got += stoneValue[i + X];
-            if (alice) {
-                procure = max(procure, got + f(mem, N, stoneValue, i + X + 1, !alice));
-            } else {
-                procure = min(procure, -got + f(mem, N, stoneValue, i + X + 1, !alice));
-            }
-        }
-        
-        return (alice ? mem[i].first : mem[i].second) = procure;
-    }
 public:
-    inline string stoneGameIII(vector<int>& stoneValue) {
+    string stoneGameIII(vector<int>& stoneValue) {
         const int N = stoneValue.size();
 
-        vector<pair<int, int>> mem(N, make_pair(1e9, 1e9));
+        vector<vector<int>> mem(N, vector<int>(2, 1e9));
+        auto f = [&] (auto&& f, int i, bool alice) {
+            if (i >= N) {
+                return 0;
+            }
 
-        int end = f(mem, N, stoneValue, 0, true);
+            if (mem[i][alice] != 1e9) {
+                return mem[i][alice];
+            } 
+
+            int procure = alice ? INT_MIN : INT_MAX;
+            int got = 0;
+            for (int X = 0; X < 3 and i + X < N; X++) {
+                got += stoneValue[i + X];
+                if (alice) {
+                    procure = max(procure, got + f(f, i + X + 1, !alice));
+                } else {
+                    procure = min(procure, -got + f(f, i + X + 1, !alice));
+                }
+            }
+
+            return mem[i][alice] = procure;
+        };
+
+        int end = f(f, 0, true);
         if (end == 0) {
             return "Tie";
         } else if (end > 0) {
