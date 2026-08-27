b5404b19944c19ff41a339d3b523405fefa3136c
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-08-27 20:36:53 +0530

    Time: 2 ms (73.25%) | Memory: 11 MB (58.60%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -8,6 +8,32 @@ public:
             occ[si - 'a']++;
         }
 
+        auto goback = [&] (string& result) {
+            int k = result.size() - 1;
+            while (k >= 0) {
+                for (int j = result.back() - 'a' + 1; j < 26; j++) {
+                    if (occ[j] > 0) {
+                        occ[result.back() - 'a']++;
+                        result.pop_back();
+
+                        result.push_back(j + 'a');
+                        occ[j]--;
+
+                        for (int x = 0; x < 26; x++) {
+                            result += string(occ[x], x + 'a');
+                        }
+                        return result;
+                    }
+                }
+            
+                occ[result.back() - 'a']++;
+                result.pop_back();
+                k--;
+            }
+
+            return string("");
+        };
+
         string result;
         for (int i = 0; i < N; i++) {
             char t = target[i];
@@ -34,91 +60,12 @@ public:
                     if (result.empty()) {
                         return "";
                     }
-
-                    // char largest = result.back();
-                    // occ[result.back() - 'a']++;
-                    // result.pop_back();
-
-                    // int k = i - 2;
-                    // while (k >= 0) {
-                    //     if (result.back() < largest) {
-                    //         occ[result.back() - 'a']++;
-                    //         result.pop_back();
-
-                    //         result.push_back(largest);
-                    //         occ[largest - 'a']--;
-
-                    //         for (int x = 0; x < 26; x++) {
-                    //             result += string(occ[x], x + 'a');
-                    //         }
-                    //         return result;
-                    //     } else {
-                    //         largest = result.back();
-                    //     }
-
-                    //     occ[result.back() - 'a']++;
-                    //     result.pop_back();
-                    //     k--;
-                    // }
-
-                    // return "";
-
-                    // occ[result.back() - 'a']++;
-                    // result.pop_back();
                     
-                    int k = i - 1;
-                    while (k >= 0) {
-                        cout << result << endl;
-                        for (int j = result.back() - 'a' + 1; j < 26; j++) {
-                            if (occ[j] > 0) {
-                                occ[result.back() - 'a']++;
-                                result.pop_back();
-
-                                result.push_back(j + 'a');
-                                occ[j]--;
-
-                                for (int x = 0; x < 26; x++) {
-                                    result += string(occ[x], x + 'a');
-                                }
-                                return result;
-                            }
-                        }
-                    
-                        occ[result.back() - 'a']++;
-                        result.pop_back();
-                        k--;
-                    }
-                    
-                    return "";
+                    break;
                 }
             }
         }
-
-        // occ[result.back() - 'a']++;
-        // result.pop_back();
         
-        int k = N - 1;
-        while (k >= 0) {
-            for (int j = result.back() - 'a' + 1; j < 26; j++) {
-                if (occ[j] > 0) {
-                    occ[result.back() - 'a']++;
-                    result.pop_back();
-
-                    result.push_back(j + 'a');
-                    occ[j]--;
-
-                    for (int x = 0; x < 26; x++) {
-                        result += string(occ[x], x + 'a');
-                    }
-                    return result;
-                }
-            }
-        
-            occ[result.back() - 'a']++;
-            result.pop_back();
-            k--;
-        }
-        
-        return "";
+        return goback(result);
     }
 };
