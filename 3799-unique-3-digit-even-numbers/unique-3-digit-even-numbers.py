08de486b26a9b761bfefe9249bd2f3019a405df8
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-09-11 20:59:33 +0530

    Time: 274 ms (9.01%) | Memory: 19.3 MB (38.46%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,12 +1,15 @@
+from collections import Counter
+
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
         N = len(digits)
 
         result = 0
+        occ = Counter(digits)
         for i in range(100, 1000, 2):
             S = str(i)
             for j in range(10):
-                if digits.count(j) < S.count(chr(j + ord('0'))):
+                if occ[j] < S.count(chr(j + ord('0'))):
                     break
             else:
                 result += 1
